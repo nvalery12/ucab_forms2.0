a57a48ad34a3072b30f224cac7f4fa23f53bca37
Angelymar Poyo <angelymar080695@gmail.com>
2022-07-26 22:40:38 -0400

    ImprimirPDF y Loader

---
+++ src/componentes/PrintFiles/PrintPDF.js
@@ -0,0 +1,38 @@
+import React, {Component, PropTypes} from 'react';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
+export default class Export extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  printDocument() {
+    const input = document.getElementById('divToPrint');
+    html2canvas(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.save("resultados.pdf");
+      })
+    ;
+  }
+
+  render() {
+    return (<div>
+      <div className="mb5">
+        <button onClick={this.printDocument}>Imprimir</button>
+      </div>
+      <div id="divToPrint" className="mt4" sx={{
+        backgroundColor: '#f5f5f5',
+        width: '210mm',
+        minHeight: '297mm',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }}>
+        <div><Post/></div> {/*AQUI SE PONE EL COMPONENTE A IMPRIMIR*/}
+      </div>
+    </div>);
+  }
+}

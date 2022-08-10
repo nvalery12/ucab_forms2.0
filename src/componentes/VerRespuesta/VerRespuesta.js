27432e038e4dd20cb18e2943e7f13e6ed59be2e8
Gustavo Vivas <tavovs5@gmail.com>
2022-08-10 04:03:34 -0400

    Funciones que no hacen nada
    no hacen nada

--- src/componentes/VerRespuesta/VerRespuesta.js
+++ src/componentes/VerRespuesta/VerRespuesta.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react';
+import React, { useState, Component, PropTypes, useEffect, useMemo } from 'react';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 import BarChart from './graficas/BarChart';
@@ -8,42 +8,147 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import PrintIcon from '@mui/icons-material/Print';
 import Button from '@mui/material/Button';
+import { useForm } from '../hooks/useForm';
+import { useNavigate } from "react-router-dom";
+import { deleteForm, getUserForms, getCollaborationForms } from '../../api/forms';
+import { useUser } from '../hooks/useUser';
+import { ConstructionOutlined } from '@mui/icons-material';
 
 import './VerRespuesta.css';
 
-export default class VerRespuestas extends Component {
-  constructor(props) {
-    super(props);
+/* Este codigo retorna un arreglo de n valores float que son
+    los porcentajes de respuesta para cada opcion de una pregunta que recibe
+    Funciona? No lo se. Tiene sentido? Probablemente no. Lo haria de nuevo? Jamas.
+    Resumen: Recibe un documento de question, cicla sobre responses y verifica el 
+    tipo de pregunta, cuenta las respuestas, las suma en un arreglo, divide, opera y chao.
+    No se como implementarlo con lo demas, auxilio
+function countSelections() {
+
+  [form, questions, respons] = useForm();
+
+  const q = questions;
+
+  let totalmuestra = 0;
+  // for (const q of questions) {
+  for (const r of response) {
+    let counts = [];
+    const ids = Object.keys(response.answer);
+    for (const o of q.opciones) {
+      counts.push(0);
+    }
+    for (let i = 0; i < ids.length; i++) {
+      if (response.answer[ids[i]] == q.id) {
+        switch (q.type) {
+          case "Selección simple":
+            for (let i = 0; i < r.length; i++) {
+              for (let i = 0; i < q.opciones.length; i++) {
+                if (r === q.opciones[i]) {
+                  counts[i]++;
+                }
+              }
+            }
+            break;
+
+          case "Selección múltiple":
+            for (let i = 0; i < r.length; i++) {
+              if (r.includes(q.opciones[i])) {
+                counts[i]++;
+              }
+            }
+            break;
+        }
+      }
+    }
+  }
+  totalmuestra++;
+  for (let i = 0; i < counts.length; i++) {
+    counts[o] = counts[o] / totalmuestra * 100;
+  }
+  //return counts;
+
+
+
+  return data.maps(x => x);
+}
+*/
+
+
+export default function VerRespuestas() {
+
+  const user = useUser();
+  const navigate = useNavigate();
+  const [userForms, setUserForms] = useState([]);
+  const [collaborationForms, setCollaborationForms] = useState([]);
+  const [loadingUserForms, setLoadingUserForms] = useState(true);
+  const [loadingCollaborationForms, setLoadingCollaborationForms] = useState(true);
+
+  useEffect(() => {
+    const unsubscribeUserForms = getUserForms(user.id, (forms) => {
+      
+      setUserForms(forms);
+      setLoadingUserForms(false);
+    });
+
+    return () => {
+      unsubscribeUserForms();
+    };
+  }, [user]);
+
+  if (loadingUserForms) {
+    return (
+      <div className='spinner'></div>
+    )      
   }
 
-  printDocument() {
-    const input = document.getElementById('divToPrint');
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF("p", "mm", "a4");
-        var width = pdf.internal.pageSize.getWidth();
-        var height = pdf.internal.pageSize.getHeight();
-        pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
-        pdf.save("resultados.pdf");
-      })
-    ;
-  }
+  const formularios = () =>{
+    let encuestas = [];
+    console.log('angy <3')
+    for (let i = 0; i < userForms.length; i++) {  
+      let form = userForms[i];
+      console.log(form);
+      for (let j = 0; j < form.length; j++) {  
+        let question = form[j];
+        console.log(question);
+      }
+    }
+    return encuestas;
+  };
 
-  respuesta_Larga() {
+
+/*  const resultadosBarChart = () => {
+
+    const data = Array(questions[1].opciones.length);
+
+
+    console.log(questions[1].opciones.length);
+    //console.log(question[1].opciones);
+
+    for (var i = 0; i < questions[1].opciones.length; i++) {
+      //console.log(question[1].opciones);
+    }
+    /*data.push({
+      name: question[1].opciones,
+      value: 0
+    });
+
+
+    return data.maps(x => x);
+  }*/
+
+  const respuesta_Larga = () => {
     return (
       <Box
         component="form"
-        className = "box question"
+        className="box question"
         noValidate
         autoComplete="off"
-        sx={{paddingBottom: "10px"}}
-        >
-        <Stack sx={{display:'flex'}}>
+        sx={{ paddingBottom: "10px" }}
+      >
+        <Stack sx={{ display: 'flex' }}>
           <p className='DescripcionPregunta'>{/*Pregunta*/}</p>
           <div className="email_answer">
             {(() => {
-              for(var i=0;i<10;i++) {
+              for (var i = 0; i < 10; i++) {
                 <>
                   <span>{/*Respuesta*/}</span>
                 </>
@@ -55,20 +160,20 @@ export default class VerRespuestas extends Component {
     )
   }
 
-  respuesta_Corta() {
+  const respuesta_Corta = () => {
     return (
       <Box
         component="form"
-        className = "box question"
+        className="box question"
         noValidate
         autoComplete="off"
-        sx={{paddingBottom: "10px"}}
-        >
-        <Stack sx={{display:'flex'}}>
+        sx={{ paddingBottom: "10px" }}
+      >
+        <Stack sx={{ display: 'flex' }}>
           <p className='DescripcionPregunta'>{/*Pregunta*/}</p>
           <div className="email_answer">
             {(() => {
-              for(var i=0;i<10;i++) {
+              for (var i = 0; i < 10; i++) {
                 <>
                   <span>{/*Respuesta*/}</span>
                 </>
@@ -80,102 +185,94 @@ export default class VerRespuestas extends Component {
     )
   }
 
-  seleccion_simple() {
-    
+  const seleccion_simple = () => {
+
     return (
       //<PieChart pieData={pData}/>
       null
     )
   }
 
-  QuestionType(type) {
-    switch(type) {
+  const QuestionType = (type) => {
+    switch (type) {
       case 'seleccionSimple':
-        return <seleccion_simple/>;
+        return <seleccion_simple />;
       case 'seleccionMultiple':
         return null;
       case 'respuestaLarga':
-        return <respuesta_Larga/>;
+        return <respuesta_Larga />;
       case 'respuestaCorta':
-        return <respuesta_Corta/>;
+        return <respuesta_Corta />;
       default:
         return;
     }
   }
 
-  
+  const pData = [
+    {
+      name: "Apple",
+      value: 54.85
+    },
+    {
+      name: "Samsung",
+      value: 47.91
+    },
+    {
+      name: "Redmi",
+      value: 16.85
+    },
+    {
+      name: "One Plus",
+      value: 16.14
+    },
+    {
+      name: "Others",
+      value: 10.25
+    }
+  ];
 
-  render() {
+  const bData = [
+    { name: 'Geeksforgeeks', students: 400 },
+    { name: 'Technical scripter', students: 700 },
+    { name: 'Geek-i-knack', students: 200 },
+    { name: 'Geek-o-mania', students: 1000 }
+  ];
 
-    const pData = [
-      {
-        name: "Apple",
-        value: 54.85
-      },
-      {
-        name: "Samsung",
-        value: 47.91
-      },
-      {
-        name: "Redmi",
-        value: 16.85
-      },
-      {
-        name: "One Plus",
-        value: 16.14
-      },
-      {
-        name: "Others",
-        value: 10.25
-      }
-    ];
-
-    const bData = [
-      { name: 'Geeksforgeeks', students: 400 },
-      { name: 'Technical scripter', students: 700 },
-      { name: 'Geek-i-knack', students: 200 },
-      { name: 'Geek-o-mania', students: 1000 }
-    ];
-
-    return (
-      <div>
-        <div id="divToPrint" className="mt4" sx={{
-          backgroundColor: '#f5f5f5',
-          width: '210mm',
-          minHeight: '297mm',
-          marginLeft: 'auto',
-          marginRight: 'auto'
-        }}>
-          <Box
-            component="form"
-            className = "box question"
-            noValidate
-            autoComplete="off"
-            sx={{paddingBottom: "10px"}}
-            >
-            <Stack sx={{display:'flex'}}>
-              <p className='DescripcionPregunta'>Respondiron:</p>
-              <div className="email_answer">
-                <span>example@example.com</span>
-                <span>example@example.com</span>
-                <span>example@example.com</span>
-              </div>
-            </Stack>
-          </Box>
-          <PieChart pieData={pData}/>
-          <BarChart barData={bData}/>
-          {(() => {
-            for(var i=0;i<10;i++) {
-              {/* {this.QuestionType(type)} */}
-            }
-          })()}
-        </div>
-        <div className="mb5">
-          <Button onClick={this.printDocument} id='btnPrint' variant="contained" color='secondary' sx={{height:'40px'}} startIcon={<PrintIcon />}>
-            Imprimir
-          </Button>
-        </div>
+  return (
+    <div>
+      <div id="divToPrint" className="mt4" sx={{
+        backgroundColor: '#f5f5f5',
+        width: '210mm',
+        minHeight: '297mm',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }}>
+        <Box
+          component="form"
+          className="box question"
+          noValidate
+          autoComplete="off"
+          sx={{ paddingBottom: "10px" }}
+        >
+          <Stack sx={{ display: 'flex' }}>
+            <p className='DescripcionPregunta'>Respondiron:</p>
+            <div className="email_answer">
+              <span>example@example.com</span>
+              <span>example@example.com</span>
+              <span>example@example.com</span>
+            </div>
+          </Stack>
+        </Box>
+        {formularios()} 
+        <PieChart pieData={pData} />
+        <BarChart barData={bData} />
+        {(() => {
+          for (var i = 0; i < 10; i++) {
+            {/* {this.QuestionType(type)} */ }
+          }
+        })()}
       </div>
-    );
-  }
+
+    </div>
+  );
 }
